35e5f7b956da6e3e5bac12db0ecd13907697c537
Thiago Matar <thiago.matar@picpay.com>
2024-07-01 18:44:59 -0300

    Adicionando public head line

---
+++ modules/users/screens/PublicProfile/PublicProfile.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts">
+import PublicHeadline from '~/modules/users/screens/components/PublicHeadline/PublicHeadline.vue'
+</script>
+
+<template>
+  <PublicHeadline class="my-10" />
+</template>
---
+++ pages/[username]/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import PublicProfile from '~/modules/users/screens/PublicProfile/PublicProfile.vue'
+import { definePageMeta } from '#imports'
+definePageMeta({
+  layout: 'default',
+})
+</script>
+
+<template>
+  <PublicProfile />
+</template>
