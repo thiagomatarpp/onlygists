54b68330948edd9017964e125554a5c3bae396db
Thiago Matar <thiago.matar@picpay.com>
2024-07-02 18:47:46 -0300

    Adicionando Widgets

--- modules/users/screens/PublicProfile/PublicProfile.vue
+++ modules/users/screens/PublicProfile/PublicProfile.vue
@@ -1,7 +1,16 @@
 <script setup lang="ts">
 import PublicHeadline from '~/modules/users/screens/components/PublicHeadline/PublicHeadline.vue'
+import WidgetGroup from '~/modules/reports/components/Widgets/Group/Group.vue'
+import WidgetCondensed from '~/modules/reports/components/Widgets/Condensed/Condensed.vue'
 </script>
 
 <template>
   <PublicHeadline class="my-10" />
+  <WidgetGroup>
+    <WidgetGroupLoader :loading="false" :amount="3">
+      <WidgetCondensed :value="5" label="Gists do total" />
+      <WidgetCondensed :value="2" label="Gists gratuitos" />
+      <WidgetCondensed :value="3" label="Gists pagos" />
+    </WidgetGroupLoader>
+  </WidgetGroup>
 </template>
